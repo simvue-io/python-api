12c089e060aae5f30ec7ffe2d01ce6a820143821
alahiff <alahiff@users.noreply.github.com>
2022-07-25 19:42:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import re
 import setuptools
 
-with open('observability/__init__.py', 'r') as fd:
+with open('simtrack/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="observability",
+    name="simtrack",
     version=version,
     author="Andrew Lahiff",
     author_email="andrew.lahiff@ukaea.uk",
-    description="Simulation management & observability",
+    description="Simulation tracking",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alahiff/observability-client",
+    url="https://github.com/alahiff/simtrack-client",
     platforms=["any"],
     install_requires=["requests"],
     package_dir={'': '.'},
-    packages=['observability'],
+    packages=['simtrack'],
     package_data={"": ["README.md"]},
 )
