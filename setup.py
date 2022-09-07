b6fe15b473e5a64b573cfb6e7f485187d8356a0e
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-09-07 16:46:43 +0100

    Rename simtrack to simvue

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import re
 import setuptools
 
-with open('simtrack/__init__.py', 'r') as fd:
+with open('simvue/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="simtrack",
+    name="simvue",
     version=version,
     author="Andrew Lahiff",
     author_email="andrew.lahiff@ukaea.uk",
     description="Simulation tracking",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alahiff/simtrack-client",
+    url="https://github.com/simvue-io/client",
     platforms=["any"],
     install_requires=["requests", "randomname"],
     package_dir={'': '.'},
-    packages=['simtrack'],
+    packages=['simvue'],
     package_data={"": ["README.md"]},
 )
