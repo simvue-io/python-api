545f192b7a2feaead83bfa3ae8cec15a6585aa09
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-10-25 12:25:43 +0000

    Check for missing config

--- tests/unit/test_simvue.py
+++ tests/unit/test_simvue.py
@@ -10,3 +10,12 @@ def test_supress_errors():
 
     with pytest.raises(RuntimeError, match="suppress_errors must be boolean"):
         simv.config(suppress_errors=200)
+
+def test_missing_config():
+    """
+    Check for missing config
+    """
+    simv = client.Simvue()
+
+    with pytest.raises(RuntimeError, match="Unable to get URL and token from environment variables or config file"):
+        simv.init()
