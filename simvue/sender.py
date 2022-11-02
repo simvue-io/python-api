2df3d500597ed53122cfa2d393d83bc44aa7266c
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-11-02 19:46:02 +0000

    Add logging

--- simvue/sender.py
+++ simvue/sender.py
@@ -1,10 +1,13 @@
 import glob
 import json
+import logging
 import os
 import tinydb
 
 from .remote import Remote
 from .utilities import get_offline_directory
+
+logger = logging.getLogger(__name__)
 
 def get_json(filename):
     """
@@ -48,15 +51,19 @@ def sender():
         results = db.search(Run.id == id)
         run_init = get_json(f"{current}/run.json")
 
+        logger.info('Considering run with namd %s and id %s', run_init['name'], id)
+
         remote = Remote(run_init['name'], suppress_errors=True)
 
         # Create run if it hasn't previously been created
         if not results:
+            logger.info('Creating run with name %s', run_init['name'])
             db.insert({'id': id, 'status': status})
             remote.create_run(run_init)
         
         # Send heartbeat if necessary
         if status == 'running':
+            logger.info('Sending heartbeat for run with name %s', run_init['name'])
             remote.send_heartbeat()
           
         # Upload metrics, events, files & metadata as necessary
@@ -72,31 +79,37 @@ def sender():
 
             # Handle metrics
             if '/metrics-' in record:
+                logger.info('Sending metrics for run %s', run_init['name'])
                 remote.send_metrics(get_binary(record))
                 rename = True
 
             # Handle events
             if '/event-' in record:
+                logger.info('Sending event for run %s', run_init['name'])
                 remote.send_event(get_binary(record))
                 rename = True
 
             # Handle updates
             if '/update-' in record:
+                logger.info('Sending update for run %s', run_init['name'])
                 remote.update(get_json(record))
                 rename = True
 
             # Handle folders
             if '/folder-' in record:
+                logger.info('Sending folder details for run %s', run_init['name'])
                 remote.set_folder_details(get_json(record))
                 rename = True
 
             # Handle alerts
             if '/alert-' in record:
+                logger.info('Sending alert details for run %s', run_init['name'])
                 remote.add_alert(get_json(record))
                 rename = True
 
             # Handle files
             if '/file-' in record:
+                logger.info('Saving file for run %s', run_init['name'])
                 remote.save_file(get_json(record))
                 rename = True
 
@@ -106,6 +119,7 @@ def sender():
 
             # Check if finished
             if record.endswith('/completed'):
+                logger.info('Run %s status changed to completed', run_init['name'])
                 db.update({'status': 'completed'}, Run.id == id)
 
     # Deal with runs in the completed state
@@ -117,4 +131,5 @@ def sender():
         if results:
             status = results[0]['status']
             if status != 'completed':
+                logger.info('Run with id %s status changed to completed', id)
                 db.update({'status': 'completed'}, Run.id == id)
