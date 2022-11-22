d9589b4d6427891c36eebc3fee6c738f9d637b17
Aby Abraham <aby.abraham@ukaea.uk>
2022-11-22 22:57:08 +0000

    Added tests to check for saving pickeable objects and files

--- tests/unit/test_simvue.py
+++ tests/unit/test_simvue.py
@@ -2,6 +2,12 @@ import os
 from simvue import Run
 import pytest
 
+import numpy as np
+import simvue.run as simvue_run
+import pickle
+
+
+
 def test_suppress_errors():
     """
     Check that errors are surpressed
@@ -10,3 +16,26 @@ def test_suppress_errors():
 
     with pytest.raises(RuntimeError, match="suppress_errors must be boolean"):
         run.config(suppress_errors=200)
+
+def test_save_input_pickle_file():
+    """
+    Pass a pickleable object e.g. nparray and check that a file is created which matches the nparray
+    """
+    arr = np.array([1,2,3,4,5])
+    
+    temp_filename = simvue_run.get_filename_input(arr, 'name_tag')
+    filehandle = open(temp_filename, 'rb')
+    assert np.array_equal(arr, pickle.load(filehandle))
+    filehandle.close()
+
+    # Delete the temp file
+    os.remove(temp_filename)  
+
+def test_save_input_file():
+    """
+    Pass a filename and check that if the file is invalid and exception is raised
+    """
+    with pytest.raises(Exception) as exc_info:
+        temp_filename = simvue_run.get_filename_input('nonexistant_file.doc', 'name_tag')
+
+    assert str(exc_info.value) == "File nonexistant_file.doc does not exist"
