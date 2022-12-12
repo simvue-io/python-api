3ce4c1865376648dc5fbce208b52e4cf06573b1a
AbyAbraham21 <aby.abraham@ukaea.uk>
2022-12-12 11:37:57 +0000

    Fix for flake8 F722 syntax error in forward annotation

--- simvue/models.py
+++ simvue/models.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel, constr
 from typing import Optional, List, Dict, Union
 from enum import Enum
 
+FolderStrRegex = constr(regex=r"^/.*")
+
 # Pydantic class to validate run.init()
 class RunInput(BaseModel):
     name: Optional[str]
     metadata: Optional[Dict[str, Union[str, int, float, None]]]
     tags: Optional[List[str]]
     description: Optional[str]
-    folder: constr(regex=r'^/.*')
+    folder: FolderStrRegex
     status: Optional[str]
