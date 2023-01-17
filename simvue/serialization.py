9409e3cf9f68c40fa74d34f9be2d22d189e4b910
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-01-17 13:08:08 +0000

    Adjustments to serialization; add deserialization

--- simvue/serialization.py
+++ simvue/serialization.py
@@ -1,12 +1,15 @@
 from io import BytesIO
 import os
 import numpy as np
+import pandas as pd
 import plotly
 
 class Serializer:
     def serialize(self, data):
         serializer = get_serializer(data)
-        return serializer(data)
+        if serializer:
+            return serializer(data)
+        return None, None
 
 def get_serializer(data):
     """
@@ -23,8 +26,7 @@ def get_serializer(data):
         return _serialize_numpy_array
     elif module_name == 'pandas.core.frame' and class_name == 'DataFrame':
         return _serialize_dataframe
-    else:
-        return None, None
+    return None
 
 def _serialize_plotly_figure(data):
     mimetype = 'application/vnd.plotly.v1+json'
@@ -51,3 +53,44 @@ def _serialize_dataframe(data):
     mfile.seek(0)
     data = mfile.read()
     return data, mimetype
+
+class Deserializer:
+    def deserialize(self, data, mimetype):
+        deserializer = get_deserializer(data, mimetype)
+        if deserializer:
+            return deserializer(data)
+        return None
+
+def get_deserializer(data, mimetype):
+    """
+    Determine which deserializer to use
+    """
+    if mimetype == 'application/vnd.plotly.v1+json':
+        return _deserialize_plotly_figure
+    elif mimetype == 'application/vnd.plotly.v1+json':
+        return _deserialize_matplotlib_figure
+    elif mimetype == 'application/vnd.simvue.numpy.v1':
+        return _deserialize_numpy_array
+    elif mimetype == 'application/vnd.simvue.df.v1':
+        return _deserialize_dataframe
+    return None
+
+def _deserialize_plotly_figure(data):
+    data = plotly.io.from_json(data)
+    return data
+
+def _deserialize_matplotlib_figure(data):
+    data = plotly.io.from_json(data)
+    return data
+
+def _deserialize_numpy_array(data):
+    mfile = BytesIO(data)
+    mfile.seek(0)
+    data = np.load(mfile, allow_pickle=False)
+    return data
+
+def _deserialize_dataframe(data):
+    mfile = BytesIO(data)
+    mfile.seek(0)
+    data = pd.read_csv(mfile)
+    return data
