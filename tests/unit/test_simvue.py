d6fc48a9351c88a40dc625f8dea4965662754de3
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-01-17 16:50:42 +0000

    Add test for pickle serialization

--- tests/unit/test_simvue.py
+++ tests/unit/test_simvue.py
@@ -134,3 +134,14 @@ def test_pytorch_tensor_serialization():
     array_out = Deserializer().deserialize(serialized, mime_type)
 
     assert (array == array_out).all()
+
+def test_pickle_serialization():
+    """
+    Check that a dictionary can be serialized then deserialized successfully
+    """
+    data = {'a': 1.0, 'b': 'test'}
+
+    serialized, mime_type = Serializer().serialize(data, allow_pickle=True)
+    data_out = Deserializer().deserialize(serialized, mime_type, allow_pickle=True)
+
+    assert (data == data_out)
