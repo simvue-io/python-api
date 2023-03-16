c62890cbe4e3c0360f03918688acd99d33d3ae22
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-03-16 17:40:06 +0000

    Add folder create, find and delete

---
+++ tests/functional/test_run_folder_find_delete.py
@@ -0,0 +1,35 @@
+import configparser
+import filecmp
+import os
+import shutil
+import time
+import unittest
+import uuid
+from simvue import Run, Client
+from simvue.sender import sender
+
+class TestRunFolder(unittest.TestCase):
+    def test_run_folder_find_delete(self):
+        """
+        Create a run & folder, find the folder then delete it
+        """
+        name = 'test-%s' % str(uuid.uuid4())
+        folder = '/tests/%s' % str(uuid.uuid4())
+        run = Run()
+        run.init(name, folder=folder)
+        run.close()
+
+        client = Client()
+        data = client.get_folder(folder)
+        self.assertEqual(data['name'], folder)
+
+        runs = client.delete_folder(folder)
+
+        client = Client()
+        with self.assertRaises(Exception) as context:
+            client.get_folder(folder)
+
+        self.assertTrue('Folder does not exist' in str(context.exception))
+
+if __name__ == '__main__':
+    unittest.main()
