27da1eee4fb74b20736720758b4506da9e5dd797
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-04-04 10:58:34 +0100

    Allow metadata & tags to be updated for runs in the created state

--- simvue/run.py
+++ simvue/run.py
@@ -389,10 +389,6 @@ class Run(object):
             self._error(INIT_MISSING)
             return False
 
-        if not self._active:
-            self._error('Run is not active')
-            return False
-
         if not isinstance(metadata, dict):
             self._error('metadata must be a dict')
             return False
@@ -413,10 +409,6 @@ class Run(object):
 
         if not self._uuid and not self._name:
             self._error(INIT_MISSING)
-            return False
-
-        if not self._active:
-            self._error('Run is not active')
             return False
 
         data = {'name': self._name, 'tags': tags}
