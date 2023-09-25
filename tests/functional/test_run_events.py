d256bbbb442fcdf4433f45bb483a44f39f24b8eb
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-09-25 06:00:06 +0000

    Support v2 and v1 server

--- tests/functional/test_run_events.py
+++ tests/functional/test_run_events.py
@@ -19,10 +19,14 @@ class TestRunEvents(unittest.TestCase):
 
         run.close()
 
+        run_id = name
+        if common.SIMVUE_API_VERSION:
+            run_id = run.id
+
         time.sleep(5)
 
         client = Client()
-        data = client.get_events(name)
+        data = client.get_events(run_id)
 
         data_compare = [{'timestamp': '2022-01-03 16:42:30.849617', 'message': 'test-event-1'},
                         {'timestamp': '2022-01-03 16:42:31.849617', 'message': 'test-event-2'}]
