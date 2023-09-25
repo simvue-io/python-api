a268bc71ff6143c044c8b102f1d18463d0628dd3
Andrew Lahiff <andrew.lahiff@gmail.com>
2023-09-25 19:02:59 +0000

    Support new backend

--- tests/functional/test_run_metrics.py
+++ tests/functional/test_run_metrics.py
@@ -21,12 +21,16 @@ class TestRunMetrics(unittest.TestCase):
 
         run.close()
 
+        run_id = name
+        if common.SIMVUE_API_VERSION:
+            run_id = run.id
+
         time.sleep(5)
 
         client = Client()
-        data_a = client.get_metrics(name, 'a', 'step')
-        data_b = client.get_metrics(name, 'b', 'step')
-        data_b_time = client.get_metrics(name, 'b', 'time')
+        data_a = client.get_metrics(run_id, 'a', 'step')
+        data_b = client.get_metrics(run_id, 'b', 'step')
+        data_b_time = client.get_metrics(run_id, 'b', 'time')
 
         data_a_val = [[0, 1.0, name, 'a'], [1, 1.2, name, 'a']]
         data_b_val = [[10, 2.0, name, 'b'], [11, 2.3, name, 'b']]
