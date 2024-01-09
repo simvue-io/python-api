1051ddc0d96ce78cb0b8d96460cd4aeae01470d9
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-01-09 15:41:31 +0000

    Revert to Union for typing

--- simvue/executor.py
+++ simvue/executor.py
@@ -54,9 +54,9 @@ class Executor:
         self,
         identifier: str,
         *args,
-        executable: str | None = None,
-        script: str | None = None,
-        input_file: str | None = None,
+        executable: typing.Optional[str]= None,
+        script: typing.Optional[str]= None,
+        input_file: typing.Optional[str]= None,
         **kwargs,
     ) -> None:
         """Add a process to be executed to the executor.
