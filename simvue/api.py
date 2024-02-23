f9d143625ea12aded711fd9302490d7a75c968ac
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-02-23 10:32:39 +0000

    Started API script refactor

--- simvue/api.py
+++ simvue/api.py
@@ -1,5 +1,6 @@
 import copy
 import json
+import typing
 import requests
 from tenacity import retry, wait_exponential, stop_after_attempt
 
@@ -9,7 +10,10 @@ RETRY_MIN = 4
 RETRY_MAX = 10
 RETRY_STOP = 5
 
-def set_json_header(headers):
+
+def set_json_header(
+    headers: dict[str, str]
+) -> dict[str, str]: 
     """
     Return a copy of the headers with Content-Type set to
     application/json
@@ -18,35 +22,115 @@ def set_json_header(headers):
     headers['Content-Type'] = 'application/json'
     return headers
 
-@retry(wait=wait_exponential(multiplier=RETRY_MULTIPLIER, min=RETRY_MIN, max=RETRY_MAX),
-                             stop=stop_after_attempt(RETRY_STOP), reraise=True)
-def post(url, headers, data, is_json=True):
-    """
-    HTTP POST with retries
+
+@retry(
+    wait=wait_exponential(
+        multiplier=RETRY_MULTIPLIER,
+        min=RETRY_MIN,
+        max=RETRY_MAX
+    ),
+    stop=stop_after_attempt(RETRY_STOP),
+    reraise=True
+)
+def post(
+    url: str,
+    headers: dict[str, str],
+    data: dict[str, typing.Any],
+    is_json: bool=True
+) -> requests.Response:
+    """HTTP POST with retries
+
+    Parameters
+    ----------
+    url : str
+        URL to post to
+    headers : dict[str, str]
+        headers for the post request
+    data : dict[str, typing.Any]
+        data to post
+    is_json : bool, optional
+        send as JSON string, by default True
+
+    Returns
+    -------
+    requests.Response
+        response from post to server
+
     """
     if is_json:
-        data = json.dumps(data)
+        data_sent: typing.Union[str, dict[str, typing.Any]] = json.dumps(data)
         headers = set_json_header(headers)
-    response = requests.post(url, headers=headers, data=data, timeout=DEFAULT_API_TIMEOUT)
+    else:
+        data_sent = data
+    
+    response = requests.post(
+        url,
+        headers=headers,
+        data=data_sent,
+        timeout=DEFAULT_API_TIMEOUT
+    )
     
     if response.status_code in (401, 403):
-        raise Exception(f'Authorization error [{response.status_code}]: {response.text}')
+        raise RuntimeError(
+            f'Authorization error [{response.status_code}]: {response.text}'
+        )
         
     if response.status_code not in (200, 201, 409):
-        raise Exception(f'HTTP error [{response.status_code}]: {response.text}')
+        raise RuntimeError(
+            f'HTTP error [{response.status_code}]: {response.text}'
+        )
 
     return response
 
-@retry(wait=wait_exponential(multiplier=RETRY_MULTIPLIER, min=RETRY_MIN, max=RETRY_MAX),
-                             stop=stop_after_attempt(RETRY_STOP))
-def put(url, headers, data, is_json=True, timeout=DEFAULT_API_TIMEOUT):
-    """
-    HTTP PUT with retries
+
+@retry(
+    wait=wait_exponential(
+        multiplier=RETRY_MULTIPLIER,
+        min=RETRY_MIN,
+        max=RETRY_MAX
+    ),
+    stop=stop_after_attempt(RETRY_STOP)
+)
+def put(
+    url: str,
+    headers: dict[str, str],
+    data: dict[str, typing.Any],
+    is_json: bool=True,
+    timeout=DEFAULT_API_TIMEOUT
+) -> requests.Response:
+    """HTTP POST with retries
+
+    Parameters
+    ----------
+    url : str
+        URL to put to
+    headers : dict[str, str]
+        headers for the post request
+    data : dict[str, typing.Any]
+        data to put
+    is_json : bool, optional
+        send as JSON string, by default True
+    timeout : _type_, optional
+        timeout of request, by default DEFAULT_API_TIMEOUT
+
+    Returns
+    -------
+    requests.Response
+        response from executing PUT
     """
     if is_json:
-        data = json.dumps(data)
+        data_sent: typing.Union[str, dict[str, typing.Any]] = json.dumps(data)
         headers = set_json_header(headers)
-    response = requests.put(url, headers=headers, data=data, timeout=timeout)
+    else:
+        data_sent = data
+    
+    response = requests.put(
+        url,
+        headers=headers,
+        data=data_sent,
+        timeout=timeout
+    )
+
     response.raise_for_status()
 
     return response
