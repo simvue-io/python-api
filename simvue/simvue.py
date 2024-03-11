8f69294b79d08d7d24c96375f4f57e78e75ba477
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-03-11 11:11:39 +0000

    Remove extra file

--- simvue/simvue.py
+++
@@ -1,9 +0,0 @@
-from .offline import Offline
-from .remote import Remote
-
-
-def Simvue(name, uuid, id, mode, suppress_errors=False):
-    if mode == "offline":
-        return Offline(name, uuid, id, suppress_errors)
-    else:
-        return Remote(name, uuid, id, suppress_errors)
