3798fca1452326473ae7158b1ddd86d3b62fbe33
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-04-02 14:19:51 +0100

    Added docstrings to Dispatcher methods

--- simvue/dispatch.py
+++ simvue/dispatch.py
@@ -1,3 +1,12 @@
+"""
+Dispatcher
+==========
+
+The Dispatcher provides a queue based system for execution of a callback on
+a list of parameters. The purpose of the class is to apply constraints to how
+often the callback can be executed, and the number of items it is called on.
+"""
+
 import logging
 import queue
 import threading
@@ -6,12 +15,19 @@ import typing
 
 MAX_REQUESTS_PER_SECOND: float = 1.0
 MAX_BUFFER_SIZE: int = 16000
+QUEUE_SIZE = 10000
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 class Dispatcher(threading.Thread):
+    """
+    The Dispatcher class enforces a maximum rate of execution for a given function
+    on items within a queue. Multiple queues can be defined with the dispatch
+    of each being executed in series. Items are added to a buffer which is handed
+    to the callback.
+    """
     def __init__(
         self,
         callback: typing.Callable[[list[typing.Any], str, dict[str, typing.Any]], None],
@@ -22,6 +38,28 @@ class Dispatcher(threading.Thread):
         max_read_rate: float = MAX_REQUESTS_PER_SECOND,
         attributes: dict[str, typing.Any] | None = None,
     ) -> None:
+        """
+        Initialise a new dispatcher
+
+        Parameters
+        ----------
+        callback : Callable[[list[Any], str, dict[str, Any]], None]
+            function to execute on queued items
+        queue_categories : list[str]
+            labels for each queue
+        queue_blocking : bool
+            whether to block queues during object to queue assignment.
+            Default is False.
+        termination_trigger : threading.Event
+            a threading event which when set declares that the dispatcher
+            should terminate
+        max_buffer_size : int
+            maximum number of items allowed in created buffer.
+        max_read_rate : float
+            maximum rate at which the callback can be executed
+        attributes : dict[str, Any]
+            additional arguments to the callback function on execution
+        """
         super().__init__()
 
         self._termination_trigger = termination_trigger
@@ -34,22 +72,29 @@ class Dispatcher(threading.Thread):
         self._queue_blocking = queue_blocking
 
     def add_item(self, item: typing.Any, queue_label: str, blocking: bool) -> None:
+        """Add an item to the specified queue with/without blocking"""
         if queue_label not in self._queues:
             raise KeyError(f"No queue '{queue_label}' found")
         self._queues[queue_label].put(item, block=blocking)
 
     @property
     def empty(self) -> bool:
+        """Returns if all queues are empty"""
         return all(queue.empty() for queue in self._queues.values())
 
     @property
     def can_send(self) -> bool:
+        """Returns if time constraints are satisfied, hence the callback can be executed"""
         if time.time() - self._send_timer >= 1 / self._max_read_rate:
             self._send_timer = time.time()
             return True
         return False
 
     def _create_buffer(self, queue_label: str) -> list[typing.Any]:
+        """Assemble queue items into a list as an argument to the callback
+        
+        The length of the buffer is constrained.
+        """
         _buffer: list[typing.Any] = []
 
         while (
@@ -63,6 +108,16 @@ class Dispatcher(threading.Thread):
         return _buffer
 
     def run(self) -> None:
+        """Execute the dispatcher action
+        
+        The action consists of a loop in which each queue is processed to
+        create a buffer with number of entries equal or less than the maximum
+        size. These are then passed into the assigned callback.
+
+        Depending on whether the user has specified to abort on trigger either
+        the loop will continue after termination until all queues are empty,
+        or abort immediately.
+        """
         while not self._termination_trigger.is_set():
             time.sleep(0.1)
             if not self.can_send:
