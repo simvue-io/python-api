66ab3ab0581680073fbb1fec32dfbd5da89ffa7d
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-05-23 09:26:21 +0100

    Do not log resource metrics on first heartbeat call if disabled

--- simvue/run.py
+++ simvue/run.py
@@ -223,7 +223,8 @@ class Run:
             last_heartbeat = time.time()
             last_res_metric_call = time.time()
 
-            self._add_metrics_to_dispatch(self._get_sysinfo(), join_on_fail=False)
+            if self._resources_metrics_interval:
+                self._add_metrics_to_dispatch(self._get_sysinfo(), join_on_fail=False)
 
             while not heartbeat_trigger.is_set():
                 time.sleep(0.1)
