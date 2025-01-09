09b7492fefcfb0c86afe086e1eed6da0e0f24956
Matt Field <matthew.field@ukaea.uk>
2025-01-09 15:53:02 +0000

    Remove url slash at end if identifier is blank

--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -3,10 +3,10 @@ import pytest
 from simvue.api.objects import Stats
 
 @pytest.mark.api
-@pytest.mark.local
+@pytest.mark.online
 def test_stats() -> None:
     _statistics = Stats()
-    assert f"{_statistics.url}" == f"{_statistics._base_url}/"
+    assert f"{_statistics.url}" == f"{_statistics._base_url}"
     assert isinstance(_statistics.runs.created, int)
     assert isinstance(_statistics.runs.running, int)
     assert isinstance(_statistics.runs.completed, int)
