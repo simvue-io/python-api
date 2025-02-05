58044bd72e5c604d81aa53fc6e5c39cb15a0f1d1
Matt Field <matthew.field@ukaea.uk>
2025-02-05 14:46:16 +0000

    added offline metric test

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -4,8 +4,9 @@ import pytest
 import time
 import datetime
 import uuid
-
+from simvue.sender import sender
 from simvue.api.objects import Metrics, Folder, Run
+from simvue import Client
 
 @pytest.mark.api
 @pytest.mark.online
@@ -44,3 +45,50 @@ def test_metrics_creation_online() -> None:
     _run.delete()
     _folder.delete(recursive=True, delete_runs=True, runs_only=False)
 
+
+@pytest.mark.api
+@pytest.mark.offline
+def test_metric_creation_offline() -> None:
+    _uuid: str = f"{uuid.uuid4()}".split("-")[0]
+    _folder_name = f"/simvue_unit_testing/{_uuid}"
+    _folder = Folder.new(path=_folder_name, offline=True)
+    _run = Run.new(folder=_folder_name, offline=True)
+    _values = {
+        "x": 1,
+        "y": 2.0,
+        "z": True
+    }
+    _time: int = 1
+    _step: int = 1
+    _folder.commit()
+    _run.commit()
+    _metrics = Metrics.new(
+        run=_run.id,
+        metrics=[
+            {
+                "timestamp": datetime.datetime.now(datetime.timezone.utc).strftime(
+                    "%Y-%m-%dT%H:%M:%S.%f"
+                ),
+                "time": _time,
+                "step": _step,
+                "values": _values,
+            }
+        ],
+        offline=True
+    )
+    _metrics.commit()
+
+    with _metrics._local_staging_file.open() as in_f:
+        _local_data = json.load(in_f)
+        
+    assert _local_data.get("run") == _run.id
+    assert _local_data.get("metrics")[0].get("values").get("x") == 1
+    assert _local_data.get("metrics")[0].get("time") == _time
+
+    _id_mapping = sender(_metrics._local_staging_file.parents[1], 1, 10, ["folders", "runs", "metrics"])
+    time.sleep(1)
+    _online_run_id = _id_mapping.get(_run.id)
+    import pdb; pdb.set_trace()
+    client = Client()
+    assert sorted(client.get_metrics_names(_online_run_id)) == sorted(list(_values.keys()))    
+    
