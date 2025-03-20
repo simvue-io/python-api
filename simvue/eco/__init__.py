08dc7a0f53e8a457234951461430b7abcd581820
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2025-03-20 16:32:01 +0000

    Format code

--- simvue/eco/__init__.py
+++ simvue/eco/__init__.py
@@ -7,6 +7,7 @@ NOTE: The metrics calculated by these methods should be used for relative
 comparisons only. Any values returned should not be taken as absolute.
 
 """
+
 __date__ = "2025-03-06"
 
 from .emissions_monitor import CO2Monitor as CO2Monitor
