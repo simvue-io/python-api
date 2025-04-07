5f047debf28b49b40e03b3fc02cc12dfbcbba385
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2025-04-07 14:59:02 +0100

    Fix count limit when retrieving objects

--- tests/unit/test_folder.py
+++ tests/unit/test_folder.py
@@ -38,20 +38,30 @@ def test_folder_creation_offline() -> None:
 
     with _folder._local_staging_file.open() as in_f:
         _local_data = json.load(in_f)
-        
+
     assert  _folder._local_staging_file.name.split(".")[0] == _folder.id
     assert _local_data.get("path", None) == _path
-        
+
     sender(_folder._local_staging_file.parents[1], 2, 10, ["folders"])
     time.sleep(1)
     client = Client()
-    
+
     _folder_new = client.get_folder(_path)
     assert _folder_new.path == _path
-    
+
     _folder_new.delete()
-    
+
     assert not _folder._local_staging_file.exists()
+
+
+@pytest.mark.api
+@pytest.mark.online
+def test_get_folder() -> None:
+    _uuid: str = f"{uuid.uuid4()}".split("-")[0]
+    _folder_name = f"/simvue_unit_testing/{_uuid}"
+    _folder_1 = Folder.new(path=f"{_folder_name}/dir_1")
+    _folder_2 = Folder.new(path=f"{_folder_name}/dir_2")
+    assert len(list(Folder._get_all_objects(count=2, offset=None))) == 2
 
 
 @pytest.mark.api
@@ -85,34 +95,34 @@ def test_folder_modification_offline() -> None:
     _tags = ["testing", "api"]
     _folder = Folder.new(path=_path, offline=True)
     _folder.commit()
-    
+
     sender(_folder._local_staging_file.parents[1], 2, 10, ["folders"])
     time.sleep(1)
-    
+
     client = Client()
     _folder_online = client.get_folder(_path)
     assert _folder_online.path == _path
-    
+
     _folder_new = Folder(identifier=_folder.id)
     _folder_new.read_only(False)
     _folder_new.tags = _tags
     _folder_new.description = _description
     _folder_new.commit()
-    
+
     with _folder._local_staging_file.open() as in_f:
         _local_data = json.load(in_f)
     assert  _folder._local_staging_file.name.split(".")[0] == _folder.id
     assert _local_data.get("description", None) == _description
     assert _local_data.get("tags", None) == _tags
-        
+
     sender(_folder._local_staging_file.parents[1], 2, 10, ["folders"])
     time.sleep(1)
-    
+
     _folder_online.refresh()
     assert _folder_online.path == _path
     assert _folder_online.description == _description
     assert _folder_online.tags == _tags
-    
+
     _folder_online.read_only(False)
     _folder_online.delete()
 
