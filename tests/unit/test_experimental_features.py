7f544ed3e702836f8dc438e21a6d51f8fd9651a8
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2026-04-17 09:35:04 +0100

    Updated test

--- tests/unit/test_experimental_features.py
+++ tests/unit/test_experimental_features.py
@@ -8,7 +8,7 @@ from simvue.api.objects import Run
 @pytest.mark.experimental
 def test_fetch_metric_values(create_test_run: tuple[Run, dict]) -> None:
     _run, _meta = create_test_run
-    for _id, _data in get_metric_values(metric_names=_meta["metrics"][:-1], run_ids=[_run.id], xaxis="step"):
+    for _id, _data in get_metric_values(metric_names=_meta["metrics"][:-1], run_ids=[_run.id], x_axis="step"):
         assert _data.get(_meta["metrics"][0])[0].get("value") is not None
         assert _data.get(_meta["metrics"][0])[0].get("step") is not None
         assert _data.get(_meta["metrics"][1])[0].get("value") is not None
