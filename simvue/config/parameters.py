52c701c07b3ff3e7da5f740c6c0563c41fee0ee8
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2026-09-21 16:12:52 +0100

    Created metadata configuration option for enabling/disabling

--- simvue/config/parameters.py
+++ simvue/config/parameters.py
@@ -18,12 +18,25 @@ from simvue.utilities import get_expiry
 
 logger = logging.getLogger(__name__)
 
+BASE_MODEL_CONFIG = pydantic.ConfigDict(extra="forbid", strict=True)
+
+
+class MetadataSpecifications(pydantic.BaseModel):
+    environment: bool = True
+    git: bool = True
+    system: bool = True
+    record_shell_vars: list[str] | None = None
+    custom: dict[str, str | int | float | bool] | None = None
+
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
+
 
 class CertificateSpecifications(pydantic.BaseModel):
     storage_ca_cert: pydantic.FilePath | bool = True
     server_ca_cert: pydantic.FilePath | bool = True
     client_cert: pydantic.FilePath | None = None
     client_key: pydantic.SecretStr | None = None
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
 
     @pydantic.model_validator(mode="before")
     @classmethod
@@ -43,10 +56,7 @@ class CertificateSpecifications(pydantic.BaseModel):
 
 
 class ServerSpecifications(pydantic.BaseModel):
-    model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(
-        extra="forbid",
-        strict=True,
-    )
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
     url: pydantic.AnyHttpUrl | None
     token: pydantic.SecretStr | None
     env: dict[str, str] | None = None
@@ -78,6 +88,7 @@ class ServerSpecifications(pydantic.BaseModel):
 
 class OfflineSpecifications(pydantic.BaseModel):
     cache: pathlib.Path | None = None
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
 
     @pydantic.field_validator("cache")
     @classmethod
@@ -94,6 +105,7 @@ class OfflineSpecifications(pydantic.BaseModel):
 class MetricsSpecifications(pydantic.BaseModel):
     system_metrics_interval: pydantic.PositiveInt | None = -1
     enable_emission_metrics: bool = False
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
 
 
 class DefaultRunSpecifications(pydantic.BaseModel):
@@ -101,10 +113,12 @@ class DefaultRunSpecifications(pydantic.BaseModel):
     description: str | None = None
     tags: list[str] | None = None
     folder: str = pydantic.Field(default="/", pattern=sv_models.FOLDER_REGEX)
-    metadata: dict[str, str | int | float | bool] | None = None
+    metadata: MetadataSpecifications = MetadataSpecifications()
     mode: typing.Literal["offline", "disabled", "online"] = "online"
     record_shell_vars: list[str] | None = None
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
 
 
 class ClientGeneralOptions(pydantic.BaseModel):
     debug: bool = False
+    model_config: typing.ClassVar[pydantic.ConfigDict] = BASE_MODEL_CONFIG
